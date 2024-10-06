8f9964ce13a965bceb53696f9ccceec7b411df4b
Johan Viancha <vianchajohan@gmail.com>
2024-10-06 15:39:25 -0500

    mover imgenes a public

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,25 +1,37 @@
+import { SearchOutlined } from "@ant-design/icons";
 import {
   Button,
   Card,
   Carousel,
   DatePicker,
   Form,
+  Image,
   InputNumber,
   Layout,
   Menu,
   Space,
-  theme,
 } from "antd";
-import { SearchOutlined } from "@ant-design/icons";
 import React from "react";
-import hotel1 from "../../assets/hotel_1.jpg";
-import logo2 from "../../assets/logo2.png";
-import hotel2 from "../../assets/hotel_2.jpg";
-import hotel3 from "../../assets/hotel_3.jpg";
-import hotel4 from "../../assets/hotel_4.jpg";
+import hotel1 from "../../../public/hotel_1.jpg";
+import hotel2 from "../../../public/hotel_2.jpg";
+import hotel3 from "../../../public/hotel_3.jpg";
+import hotel4 from "../../../public/hotel_4.jpg";
+import logo3 from "../../../public/logo3.png";
+
 import "./Home.css";
 
 const { Header, Content, Footer } = Layout;
+
+const menuItems = [
+  {
+    key: "1",
+    label: "Servicios",
+  },
+  {
+    key: "2",
+    label: "Reservar",
+  },
+];
 const items = new Array(3).fill(null).map((_, index) => ({
   key: String(index + 1),
   label: `nav ${index + 1}`,
@@ -29,41 +41,41 @@ const { RangePicker } = DatePicker;
 const dateFormat = "DD/MM/YYYY";
 
 const Home = () => {
-  const {
-    token: { colorBgContainer, borderRadiusLG },
-  } = theme.useToken();
   return (
     <Layout>
       <Header className="header">
-        <img className="header__logo" src={logo2} alt="Logo" />
+        <img className="header__logo" src={logo3} alt="Logo" />
         <Menu
           theme="dark"
           className="header__menu"
           mode="horizontal"
           defaultSelectedKeys={["1"]}
-          items={items}
+          items={menuItems}
           style={{
             flex: 1,
             minWidth: 0,
           }}
         />
       </Header>
+      <div className="container-carousel img-1"></div>
 
       <Content className="content">
-        <Carousel arrows dotPosition="left" infinite={false}>
-          <div className="container-carousel">
-            <img className="container-carousel__img" src={hotel1} alt="" />
-          </div>
-          <div className="container-carousel">
-            <img className="container-carousel__img" src={hotel2} alt="" />
-          </div>
-          <div className="container-carousel">
-            <img className="container-carousel__img" src={hotel3} alt="" />
-          </div>
-          <div className="container-carousel">
-            <img className="container-carousel__img" src={hotel4} alt="" />
-          </div>
-        </Carousel>
+        <div className="slides">
+          <Carousel arrows dotPosition="left" infinite={false} autoplay>
+            <div className="container-carousel">
+              <img src={hotel1} />
+            </div>
+            <div className="container-carousel">
+              <img src={hotel2} />
+            </div>
+            <div className="container-carousel">
+              <img src={hotel3} />
+            </div>
+            <div className="container-carousel">
+              <img src={hotel4} />
+            </div>
+          </Carousel>
+        </div>
 
         <Card bordered={false} className="card-reservation">
           <Form className="form-reservation" name="control-hooks">
@@ -82,18 +94,8 @@ const Home = () => {
               className="form-reservation__item"
             >
               <div className="card-reservation__travelers">
-                <InputNumber
-                  placeholder="Adultos"
-                  min={1}
-                  max={10}
-                  defaultValue={""}
-                />
-                <InputNumber
-                  placeholder="Niños"
-                  min={1}
-                  max={10}
-                  defaultValue={""}
-                />
+                <InputNumber placeholder="Adultos" min={1} max={10} />
+                <InputNumber placeholder="Niños" min={1} max={10} />
               </div>
             </Form.Item>
 
@@ -102,12 +104,7 @@ const Home = () => {
               label="Habitaciones"
               className="form-reservation__item"
             >
-              <InputNumber
-                placeholder="Habitaciones"
-                min={1}
-                max={10}
-                defaultValue={""}
-              />
+              <InputNumber placeholder="Habitaciones" min={1} max={10} />
             </Form.Item>
 
             <Form.Item className="form-reservation__item">
